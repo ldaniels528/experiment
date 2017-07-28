356254006a0b7d427dc140b9b9dede93cda22274
Lawrence Daniels <lawrence.daniels@gmail.com>
2017-07-28 15:25:39 -0700

    Continued work on the ETL

--- app/jvm/etl/src/main/scala/com/github/ldaniels528/qwery/etl/actors/FileManagementActor.scala
+++ app/jvm/etl/src/main/scala/com/github/ldaniels528/qwery/etl/actors/FileManagementActor.scala
@@ -3,24 +3,28 @@ package actors
 
 import java.io.File
 import java.nio.file.StandardWatchEventKinds.{ENTRY_CREATE, ENTRY_MODIFY}
-import java.nio.file.{Paths, WatchKey}
+import java.nio.file._
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.util.concurrent.TimeUnit
 
 import akka.actor.{Actor, ActorLogging}
 import com.github.ldaniels528.qwery.actors.QweryActorSystem
 import com.github.ldaniels528.qwery.etl.actors.FileManagementActor._
 import com.github.ldaniels528.qwery.util.DurationHelper._
+import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 import scala.collection.concurrent.TrieMap
 import scala.concurrent.duration._
+import scala.util.Try
 
 /**
   * File Management Actor
   * @author lawrence.daniels@gmail.com
   */
 class FileManagementActor(config: ETLConfig) extends Actor with ActorLogging {
+  private val logger = LoggerFactory.getLogger(getClass)
   private val watchedFiles = TrieMap[String, WatchedFile]()
   private implicit val dispatcher = context.dispatcher
 
@@ -53,26 +57,31 @@ class FileManagementActor(config: ETLConfig) extends Actor with ActorLogging {
     }
   }
 
-  private def registerWatch(directory: File, callback: FileWatchCallback) = {
-    val path = Paths.get(directory.getAbsolutePath)
-    val watcher = path.getFileSystem.newWatchService()
-    Seq(ENTRY_CREATE, ENTRY_MODIFY) foreach (path.register(watcher, _))
+  private def registerWatch(directory: File, callback: FileWatchCallback): Unit = {
+    val path = directory.getCanonicalFile.toPath
+    val watchService = FileSystems.getDefault.newWatchService
+    path.register(watchService, ENTRY_CREATE, ENTRY_MODIFY)
 
-    var watchKey: Option[WatchKey] = None
-    QweryActorSystem.scheduler.schedule(0.seconds, 1.second) {
-      if (watchKey.isEmpty) watchKey = Option(watcher.poll())
-      else {
-        watchKey.foreach(_.pollEvents().asScala.foreach { event =>
-          Option(event.context()).map(_.toString) match {
-            case Some(content) =>
-              val file = new File(directory, content)
-              if (!watchedFiles.contains(file.getCanonicalPath)) {
-                callback(file)
-              }
-            case None =>
-              log.info("Context was null")
-          }
-        })
+    def run(file: File): Option[Try[Unit]] = {
+      if (!watchedFiles.contains(file.getAbsolutePath)) {
+        val result = Try(callback(file))
+        watchedFiles.remove(file.getAbsolutePath)
+        Some(result)
+      }
+      else None
+    }
+
+    // look for new files every 30 seconds
+    QweryActorSystem.scheduler.schedule(1.seconds, 5.seconds) {
+      for {watchKey <- Option(watchService.poll(1, TimeUnit.SECONDS))} {
+        watchKey.pollEvents.asScala foreach { event =>
+          val file = path.resolve(event.context.asInstanceOf[Path]).toFile
+          run(file)
+        }
+
+        // reset the key
+        val valid = watchKey.reset
+        if (!valid) logger.error("Watch Key has been unregistered")
       }
     }
 
@@ -80,12 +89,12 @@ class FileManagementActor(config: ETLConfig) extends Actor with ActorLogging {
     Option(directory.listFiles()) foreach { files =>
       if (files.nonEmpty) {
         log.info(s"Processing ${files.length} pre-existing files...")
-        for (file <- files) callback(file)
+        files foreach run
       }
     }
   }
 
-  private def storeFile(fileToStore: File, compress: Boolean): Boolean = {
+  private def storeFile(fileToStore: File, compress: Boolean) = {
     fileToStore match {
       case file if file.isFile && file.lastModified() > System.currentTimeMillis() - 15.seconds =>
         log.info(s"File '${file.getName}' is not at least 15 seconds old")
