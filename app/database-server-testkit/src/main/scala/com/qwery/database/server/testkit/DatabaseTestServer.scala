cf5b8bda7963d8bc1a6bc31aeb0f5be616c2aafb
Lawrence Daniels <lawrence.daniels@coxautoinc.com>
2021-02-04 19:49:50 -0800

    removed QueryProcessor references

--- app/database-server-testkit/src/main/scala/com/qwery/database/server/testkit/DatabaseTestServer.scala
+++ app/database-server-testkit/src/main/scala/com/qwery/database/server/testkit/DatabaseTestServer.scala
@@ -1,11 +1,8 @@
 package com.qwery.database.server.testkit
 
 import akka.actor.ActorSystem
-import akka.util.Timeout
-import com.qwery.database.server.{DatabaseServer, QueryProcessor}
+import com.qwery.database.server.DatabaseServer
 import org.slf4j.LoggerFactory
-
-import scala.concurrent.duration._
 
 /**
  * Database Test Server
@@ -13,10 +10,8 @@ import scala.concurrent.duration._
 object DatabaseTestServer {
   private val logger = LoggerFactory.getLogger(getClass)
 
-  def startServer(port: Int, timeout: Timeout = 2.minutes): Unit = {
+  def startServer(port: Int): Unit = {
     implicit val system: ActorSystem = ActorSystem(name = "test-server")
-    implicit val _timeout: Timeout = timeout
-    implicit val queryProcessor: QueryProcessor = new QueryProcessor()
     import system.dispatcher
 
     logger.info(s"Starting Test Database Server on port $port...")
